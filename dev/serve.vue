2c27d3a8285f8d610200ff25a9df41d2fa9f7627
LasanthaSudeera <lasan.sudeera@gmail.com>
2020-12-30 19:00:43 +0530

    Initial Commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import VueVcard from '@/vue-vcard.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    VueVcard
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <vue-vcard />
+  </div>
+</template>
---
+++ src/vue-vcard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="vue-vcard">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VueVcard', // vue component name
+  data() {
+    return {
+
+    };
+  },
+  computed: {
+   
+  },
+  methods: {
+    
+  
+  },
+};
+</script>
+
+<style scoped>
+  .vue-vcard {
+    display: block;
+    width: 400px;
+    margin: 25px auto;
+    border: 1px solid #ccc;
+    background: #eaeaea;
+    text-align: center;
+    padding: 25px;
+  }
+  .vue-vcard p {
+    margin: 0 0 1em;
+  }
+</style>
