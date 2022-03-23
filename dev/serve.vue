8f4c8b634891be2048910e7df789e212081cdff4
udesh265 <udeshika265@gmail.com>
2022-03-23 16:43:35 +0530

    change city name

--- dev/serve.vue
+++ dev/serve.vue
@@ -1,17 +1,28 @@
 <script>
-import Vue from 'vue';
-import VueVcard from '@/vue-vcard.vue';
+import Vue from "vue";
+import VueVcard from "@/vue-vcard.vue";
 
 export default Vue.extend({
-  name: 'ServeDev',
+  name: "ServeDev",
   components: {
-    VueVcard
+    VueVcard,
   },
 });
 </script>
 
 <template>
   <div id="app">
-    <vue-vcard orgPost="00000" orgStreet="Some Street" orgRegion="Some Region" orgCity="Some City" orgCountry="LRK" orgName="OrganizationName" firstName="John" lastName="Doe" workPhone="5555555555"  homePhone="5555555555" />
+    <vue-vcard
+      orgPost="00000"
+      orgStreet="Some Street name"
+      orgRegion="Some Region"
+      orgCity="Some City"
+      orgCountry="LRK"
+      orgName="OrganizationName"
+      firstName="John"
+      lastName="Doe"
+      workPhone="5555555555"
+      homePhone="5555555555"
+    />
   </div>
 </template>
